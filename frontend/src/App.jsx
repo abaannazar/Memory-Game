15544e693645bcdf8adcf23e9dfaa787606f2fbb
Abaan Nazar <84410795+abaannazar@users.noreply.github.com>
2025-05-22 13:15:09 +0530

    Updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MemoryGame from './components/memory game'
+
+const App = () => {
+  return (
+    <div>
+      <MemoryGame/>
+    </div>
+
+  )
+}
+
+export default App
